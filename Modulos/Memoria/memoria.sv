58ddb48fbf57cd686b7e5f40b58e7a22737682f2
Freddymb7 <freddy.mora.bolanos@gmail.com>
2024-09-25 20:54:21 -0600

    Add files via upload
    También contiene un ejemplo .texto para probar la memoria

---
+++ Modulos/Memoria/memoria.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 25.09.2024 19:06:14
+// Design Name: 
+// Module Name: memoria
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module memoria(
+
+    );
+endmodule
