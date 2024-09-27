72a4fea733082539e41a9a62be826820c09dd9f2
Freddymb7 <freddy.mora.bolanos@gmail.com>
2024-09-26 20:55:17 -0600

    Add files via upload
    Se añadieron las memorias, solo falta las intrucciones especificas

---
+++ Modulos/Memoria/memoria.sv
@@ -0,0 +1,28 @@
+`timescale 1ns / 1ps
+
+module memoria_datos(input         clk, we, // We = Write data
+            input  [63:0] a, wd,           // a = adress, wd =write data
+            output [63:0] rd);             // read data
+
+  reg  [63:0] RAM[63:0]; 
+
+  assign rd = RAM[a[31:2]]; // word aligned
+// para agarrar parte superior [63:32] y la parte inferior [31:0]
+  always @(posedge clk)
+    if (we)
+      RAM[a[31:2]] <= wd;
+endmodule
+
+module memoria_instrucciones(input  [5:0] a,
+            output [63:0] rd);
+    
+  reg  [63:0] RAM[63:0];
+
+initial begin
+        $display("Loading rom.");
+        $readmemh("C:/Users/Estudiante/project_2/project_2.srcs/sources_1/new/memoria.mem", RAM);
+    end
+  assign rd = RAM[a]; // word aligned
+endmodule
+
+
---
+++ Modulos/Memoria/testbench.sv
@@ -0,0 +1,41 @@
+`timescale 1ns / 1ps
+module memoria_datostb( 
+); 
+
+reg we;
+reg [5:0] a;
+reg [63:0] wd;
+wire [63:0] rd;
+reg clk;
+reg [63:0] a1;
+wire [63:0] rd1;
+memoria_datos s0(.clk(clk),
+.we(we),
+.a(a1),
+.wd(wd),
+.rd(rd1)
+);
+
+memoria_instrucciones m0(.a(a),
+.rd(rd)
+); 
+
+always #1 clk = ~clk;
+  initial begin
+    we = 0; 
+    a = 0; 
+    wd = 0;  
+    clk = 0;
+    a1 =0; 
+#10 
+a = 1; 
+#10 
+$display("instruccion=%h",rd);
+#10 
+$finish; 
+    end
+initial begin
+        $dumpfile("testbench.vcd");
+        $dumpvars(0,memoria_datostb);
+end
+endmodule
